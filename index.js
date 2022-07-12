95fc535af4b265e69e75af98d215e4f4abcd634d
omer <jagenau49@gmail.com>
2022-07-12 14:39:17 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const port = 3001;
+
+app.get('/',(req,res)=>{
+    res.send("Welcome my API!");
+})
+app.listen(port, () => {
+  console.log(`Server runs on: http://localhost:${port}`);
+});
