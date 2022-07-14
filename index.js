6dad4a4331eb04f015991d9d99f5f3f593e1e7ad
omer <jagenau49@gmail.com>
2022-07-14 12:12:18 +0200

    gets data from local

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { JobSource } from "./models/JobSource.js";
+
+dotenv.config();
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lc-job-manager";
+mongoose.connect(MONGODB_URI, (err) => {
+  if (err) {
+    console.log({
+      error: "Cannot connect to MongoDB database.",
+      err: `${err}`,
+    });
+  }
+});
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-app.get('/',(req,res)=>{
-    res.send("Welcome my API!");
-})
+app.get("/", (req, res) => {
+  res.send("Welcome my API!");
+});
+app.get("/job-sources", async (req, res) => {
+  const jobSources = await JobSource.find();
+ 
+  res.status(200).json(jobSources);
+});
 app.listen(port, () => {
   console.log(`Server runs on: http://localhost:${port}`);
 });
